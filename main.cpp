e734ed87a670bfa29d4a3736653ee73e92398f52
Sala34_uczen <moskalov.yevhen@zst.kolbuszowa.pl>
2023-03-23 11:48:01 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    int y = 3;
+
+    if (x > y) {
+        std::cout << ">" << std::endl;
+    }
+    else {
+        std::cout << "blad" << std::endl;
+    }
+
+    return 0;
+}
